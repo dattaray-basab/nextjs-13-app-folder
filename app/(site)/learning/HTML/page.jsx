0f066c4662217722d0c488fffed39b7466667513
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-05 15:49:35 -0800

    0.0.5: WIP - added Atef's content

--- app/(site)/learning/HTML/page.jsx
+++ app/(site)/learning/HTML/page.jsx
@@ -1,11 +1,44 @@
+import Link from 'next/link'
+
 const Page = () => {
   return (
     <section>
       <div className='px-4 py-4 sm:px-6 lg:px-8'>
         <h1 className='mb-3 text-2xl font-semibold'>HTML</h1>
-        <div className='flex items-center gap-2'>
-          <span className='text-2xl'>👈🏼</span>
-          <span>regarding html</span>
+        <div>
+          <h1>Class View</h1>
+          <br></br>
+
+          <p>
+            <strong>First Lesson</strong> Segment: What is HTML and how to use
+            it Make Video On This
+          </p>
+
+          <br></br>
+
+          <p>
+            The <strong>Second Segment </strong>
+            <Link href={'https://www.youtube.com/watch?v=bWPMSSsVdPk'}>
+            Video
+            </Link>{' '}
+            watch till 3:30.
+          </p>
+          <br></br>
+
+          <p>
+            Quiz: What do tags do? What is the code for header tag? Body tag?
+            What are the differences between the two and what goes where?
+          </p>
+          <br></br>
+
+          <p>
+            <strong>Third Segment: </strong>
+            <Link href={'https://www.youtube.com/watch?v=qz0aGYrrlhU'}>
+              Video
+            </Link>{' '}
+            watch from 22:40 till 29:55. Learn to use header, font, bold, and
+            color and Inserting images into
+          </p>
         </div>
       </div>
     </section>
