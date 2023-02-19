cc9dcd4ef05b0508aeeb06422deb77bfb03e7302
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-02-18 19:17:53 -0800

    0.1.1: START - adding pages

--- app/(site)/about/page.jsx
+++ app/(site)/about/page.jsx
@@ -3,10 +3,7 @@ const Page = () => {
     <section>
       <div className='px-4 py-4 sm:px-6 lg:px-8'>
         <h1 className='mb-3 text-2xl font-semibold'>About</h1>
-        <div className='flex items-center gap-2'>
-          <span className='text-2xl'>👈🏼</span>
-          <span>select a user</span>
-        </div>
+
       </div>
     </section>
   )
